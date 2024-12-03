c32915fd1dec8a771c73256c60a2681474ef9a9b
zvolcan <emir49499141@gmail.com>
2024-12-03 11:19:28 -0300

    initial commit

---
+++ src/main/java/org/zvolcan/uhccorestats/UHCcore_Stats.java
@@ -0,0 +1,17 @@
+package org.zvolcan.uhccorestats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UHCcore_Stats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
